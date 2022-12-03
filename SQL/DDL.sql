5e60be4bccb9e3ecaab2866efbca9e3a5e2ea8e4
abbas1902 <abbas1902@hotmail.ca>
2022-12-03 14:46:51 -0500

    some preliminary work with sqllite and added db

--- SQL/DDL.sql
+++ SQL/DDL.sql
@@ -1,6 +1,20 @@
 create table user
 	(
-     username   varchar(20),
-     password   varchar(20),
+     username   varchar(20) not null,
+     password   varchar(20) not null,
+     isManager char(1),
      primary key(username, password)
 	);
+
+create table books(
+     ISBN int(10) not null,
+     title varchar (50) not null,
+     author varchar (20) not null,
+     yearPublished int(4) not null,
+     publisher varchar (20) not null, 
+     genre varchar (20) not null,
+     pageNumber int (4) not null,
+     price float (5) not null,
+     primary key(ISBN)
+);
+
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const session = require('express-session');
+const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
 
@@ -16,6 +17,14 @@ app.use("/logout", require('./routers/logout-router.js'));
 //Start server
 try{
 	app.listen(3000);
+	//database
+	let booksDB = new sqlite3.Database('.\SQL\books.db',sqlite3.OPEN_READWRITE, (err) => {
+		if (err) {
+		  console.error(err.message);
+		}
+		console.log('Connected to the book database.');
+	  });
+
 	console.log("Server listening at http://localhost:3000");
 }catch(err){
 	console.log(err);
