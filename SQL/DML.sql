4fcad4f30548e66f495d1a36decdba73cdb87bb8
Ewashere0 <58784516+Ewashere0@users.noreply.github.com>
2022-12-11 01:04:41 -0500

    small fix for DML

--- SQL/DML.sql
+++ SQL/DML.sql
@@ -8,13 +8,13 @@ insert into authors
 values ('jkrowling@gmail.com', 'J. K. Rowling');
 
 insert into books
-values (1, "Harry Potter and the Philosopher's Stone", 1997, 'Bloomberg', 223, 9.99, 10, null, null),
-       (2, "Harry Potter and the Chamber of Secrets", 1998, 'Bloomberg', 251, 9.99, 10, null, null),
-       (3, "Harry Potter and the Prisoner of Azkaban", 1999, 'Bloomberg', 317, 9.99, 10, null, null),
-       (4, "Harry Potter and the Goblet of Fire", 2000, 'Bloomberg', 636, 9.99, 10, null, null),
-       (5, "Harry Potter and the Order of the Pheonix", 2003, 'Bloomberg', 766, 9.99, 10, null, null),
-       (6, "Harry Potter and the Half-Blood Prince", 2005, 'Bloomberg', 607, 9.99, 10, null, null),
-       (7, "Harry Potter and the Deathly Hallows", 2007, 'Bloomberg', 607, 9.99, 10, null, null);
+values (1, 'Harry Potter and the Philosophers Stone', 1997, 'Bloomberg', 223, 9.99, 10, null, null),
+       (2, 'Harry Potter and the Chamber of Secrets', 1998, 'Bloomberg', 251, 9.99, 10, null, null),
+       (3, 'Harry Potter and the Prisoner of Azkaban', 1999, 'Bloomberg', 317, 9.99, 10, null, null),
+       (4, 'Harry Potter and the Goblet of Fire', 2000, 'Bloomberg', 636, 9.99, 10, null, null),
+       (5, 'Harry Potter and the Order of the Pheonix', 2003, 'Bloomberg', 766, 9.99, 10, null, null),
+       (6, 'Harry Potter and the Half-Blood Prince', 2005, 'Bloomberg', 607, 9.99, 10, null, null),
+       (7, 'Harry Potter and the Deathly Hallows', 2007, 'Bloomberg', 607, 9.99, 10, null, null);
 
 insert into writes
 values (1, 'jkrowling@gmail.com'),
@@ -26,15 +26,15 @@ values (1, 'jkrowling@gmail.com'),
        (7, 'jkrowling@gmail.com');
 
 insert into genres
-values (1, 'Fantasy')
-       (2, 'Fantasy')
-       (3, 'Fantasy')
-       (4, 'Fantasy')
-       (5, 'Fantasy')
-       (6, 'Fantasy')
-       (7, 'Fantasy')
+values (1, 'Fantasy'),
+       (2, 'Fantasy'),
+       (3, 'Fantasy'),
+       (4, 'Fantasy'),
+       (5, 'Fantasy'),
+       (6, 'Fantasy'),
+       (7, 'Fantasy');
 
 insert into users
-values ('abbas', 'abbas', 'Owner')
-       ('adam', 'adam', 'Owner')
-       ('ethan', 'ethan', 'Owner')
+values ('abbas', 'abbas', 'Owner'),
+       ('adam', 'adam', 'Owner'),
+       ('ethan', 'ethan', 'Owner');
