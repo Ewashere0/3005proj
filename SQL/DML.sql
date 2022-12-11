e641dd70e20e160a9d80002e57fa0a71cdc51582
Ewashere0 <58784516+Ewashere0@users.noreply.github.com>
2022-12-11 18:27:55 -0500

    Ethan - addbook changes (#10)
    * added DML to initialiser, modified DDL to look like ER which will break addBook-router.js
    
    * small fix for DML
    
    * modify addbook to work with new DB schema

---
+++ SQL/DML.sql
@@ -0,0 +1,40 @@
+insert into publishers(name, email, address, bankAccount)
+values ('Bloomberg', 'bloomberg@gmail.com', '31 Bedford Avenue, London', 1234);
+
+insert into phoneNumbers
+values (1234568790, 1);
+
+insert into authors
+values ('jkrowling@gmail.com', 'J. K. Rowling');
+
+insert into books
+values (1, 'Harry Potter and the Philosophers Stone', 1997, 'Bloomberg', 223, 9.99, 10, null, 1),
+       (2, 'Harry Potter and the Chamber of Secrets', 1998, 'Bloomberg', 251, 9.99, 10, null, 1),
+       (3, 'Harry Potter and the Prisoner of Azkaban', 1999, 'Bloomberg', 317, 9.99, 10, null, 1),
+       (4, 'Harry Potter and the Goblet of Fire', 2000, 'Bloomberg', 636, 9.99, 10, null, 1),
+       (5, 'Harry Potter and the Order of the Pheonix', 2003, 'Bloomberg', 766, 9.99, 10, null, 1),
+       (6, 'Harry Potter and the Half-Blood Prince', 2005, 'Bloomberg', 607, 9.99, 10, null, 1),
+       (7, 'Harry Potter and the Deathly Hallows', 2007, 'Bloomberg', 607, 9.99, 10, null, 1);
+
+insert into writes
+values (1, 'jkrowling@gmail.com'),
+       (2, 'jkrowling@gmail.com'),
+       (3, 'jkrowling@gmail.com'),
+       (4, 'jkrowling@gmail.com'),
+       (5, 'jkrowling@gmail.com'),
+       (6, 'jkrowling@gmail.com'),
+       (7, 'jkrowling@gmail.com');
+
+insert into genres
+values (1, 'Fantasy'),
+       (2, 'Fantasy'),
+       (3, 'Fantasy'),
+       (4, 'Fantasy'),
+       (5, 'Fantasy'),
+       (6, 'Fantasy'),
+       (7, 'Fantasy');
+
+insert into users
+values ('abbas', 'abbas', 'Owner'),
+       ('adam', 'adam', 'Owner'),
+       ('ethan', 'ethan', 'Owner');
--- routers/addBook-router.js
+++ routers/addBook-router.js
@@ -16,51 +16,164 @@ router
     .get('/', getForm)
     .put('/', express.json(), addBook)
 
-    function getForm(req, res, next){
-        if(!req.session.loggedin){
-            res.status(404).send('You must be logged in to access this page');
-            return;
-        }
-        if(!req.session.owner){
-            res.status(403).send('Users cannot access this page');
-            return;
-        }
-        let data = pug.renderFile("views/pages/addBook.pug", {
+function getForm(req, res, next){
+    if(!req.session.loggedin){
+        res.status(404).send('You must be logged in to access this page');
+        return;
+    }
+    if(!req.session.owner){
+        res.status(403).send('Users cannot access this page');
+        return;
+    }
+    res.format({
+        "text/html": () => {res.render("../views/pages/addBook.pug", {
           loggedin: req.session.loggedin, 
           username: req.session.username,
           owner: req.session.owner
-        });
-        res.setHeader('Content-Type', 'text/html');
-        res.status(200).send(data);
-        return;
+        })}
+    });
+}
+
+function addBook(req, res, next){
+    let data = {
+        'ISBN': req.body.ISBN,
+        'Title': req.body.title,
+        'Author': req.body.author,
+        'AuthorEmail': req.body.authorEmail,
+        'Year': req.body.year,
+        'Publisher':req.body.publisher,
+        'Genre': req.body.genre,
+        'Pages': req.body.pageNum,
+        'Price': req.body.price,
+        'Quantity': req.body.quantity,
     }
-    
-    function addBook(req, res, next){
-        let data = {
-            'ISBN': req.body.ISBN,
-            'Title': req.body.title,
-            'Year': req.body.year,
-            'Publisher':req.body.publisher,
-            'Pages': req.body.pageNum,
-            'Price': req.body.price,
-            'Quantity': req.body.quantity,
-            'salePercentage': req.body.salePercentage
+    const text = 'SELECT publisherid FROM publishers WHERE name = $1'
+    const values = [data.Publisher]
+
+    pool.connect((err, client, done) => {
+      if (err) throw err
+      client.query(text, values, (err, res) => {
+        if (err) {
+          console.log(err.stack)
+        } else {
+          if (res.rowCount !== 0 ){
+            console.log("found publisher id for " + data.Publisher)
+            data.PublisherID = (res.rows[0].publisherid);
+            insertBook(data);
+          }
+          else{
+            insertPublisher(data);
+          }
         }
-        const text = 'INSERT INTO books VALUES($1, $2, $3, $4, $5, $6, $7, $8)'
-        const values = [data.ISBN,data.Title,data.Year,data.Publisher,data.Pages,data.Price,data.Quantity,date.salePercentage]
+        client.release();
+      })
+    })
+}
+
+function insertPublisher(data){
+  const text = 'insert into publishers(name, email, address, bankAccount) values($1, $2, $3, $4)'
+  const values = [data.Publisher, null, null, null]
+
+  pool.connect((err, client, done) => {
+    if (err) throw err
+    client.query(text, values, (err, res) => {
+      if (err) {
+        console.log(err.stack)
+      } else {
+        console.log('added publisher ' + data.Publisher)    
+        
+        const text = 'SELECT publisherid FROM publishers WHERE name = $1'
+        const values = [data.Publisher]
+
         pool.connect((err, client, done) => {
-            if (err) throw err
-            client.query(text, values, (err, res) => {
-              if (err) {
-                console.log(err.stack)
-              } else {
-                console.log('success!')
-              }
-            })
+          if (err) throw err
+          client.query(text, values, (err, res) => {
+            if (err) {
+              console.log(err.stack)
+            } else {
+              data.PublisherID = res.rows[0].publisherid
+              insertBook(data);
+            }
+            client.release();
           })
-        console.log(data)
-    
-        res.status(201).send(data)
-    }
+        })
+      }
+      client.release();
+    })
+  })
+}
+
+function insertBook(data){
+  text = 'INSERT INTO books VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)'
+  values = [data.ISBN,data.Title,data.Year,data.Publisher,data.Pages,data.Price,data.Quantity, null, data.PublisherID]
+
+  pool.connect((err, client, done) => {
+      if (err) throw err
+      client.query(text, values, (err, res) => {
+        if (err) {
+          console.log(err.stack)
+        } else {
+          console.log('added book ' + data.Title)
+          insertGenre(data);
+        }
+        client.release();
+      })
+    })
+}
+
+function insertGenre(data){
+  text = 'INSERT INTO genres VALUES($1, $2)'
+  values = [data.ISBN, data.Genre]
+
+  pool.connect((err, client, done) => {
+      if (err) throw err
+      client.query(text, values, (err, res) => {
+        if (err) {
+          console.log(err.stack)
+        } else {
+          console.log('added genre ' + data.Genre)
+          insertAuthor(data);
+        }
+        client.release();
+      })
+    })
+}
+
+function insertAuthor(data){
+  text = 'INSERT INTO authors VALUES($1, $2)'
+  values = [data.AuthorEmail, data.Author]
+
+  pool.connect((err, client, done) => {
+      if (err) throw err
+      client.query(text, values, (err, res) => {
+        if (err) {
+          console.log(err.stack)
+        } else {
+          console.log('added author ' + data.Author)
+          insertWrites(data);
+        }
+        client.release();
+      })
+    })
+}
+
+function insertWrites(data){
+  text = 'INSERT INTO writes VALUES($1, $2)'
+  values = [data.ISBN, data.AuthorEmail]
+
+  pool.connect((err, client, done) => {
+      if (err) throw err
+      client.query(text, values, (err, res) => {
+        if (err) {
+          console.log(err.stack)
+        } else {
+          console.log('added writes ' + data.AuthorEmail)
+        }
+        client.release();
+
+      })
+    })
+}
+
 //Export the router so it can be mounted in the main app
 module.exports = router;
