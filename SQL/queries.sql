b22189d55e02d680ccaf375b00f3485d13ee827f
Ewashere0 <58784516+Ewashere0@users.noreply.github.com>
2022-12-11 23:24:51 -0500

    add querries from order-router.js to queries.sql (#17)

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -13,6 +13,12 @@ SELECT * FROM users WHERE username = $1 AND password = $2
 
 From 'routers\order-router.js'
 SELECT ISBN,title FROM books WHERE inventory > 0;
+SELECT address,cardNumber,name FROM billingInfo WHERE username = $1;
+SELECT ISBN,title,price,inventory FROM books WHERE inventory > 0;
+INSERT INTO billingInfo VALUES($1, $2, $3, $4)
+INSERT INTO orders(shippingAddress, orderDate, trackingInfo, totalAmount, cardNumber)\
+	VALUES($1, to_timestamp($2), $3, $4, $5) RETURNING orderNumber
+INSERT INTO contains VALUES($1, $2, $3)
 
 From 'routers\registration-router.js'
 INSERT INTO users VALUES($1, $2, $3)
