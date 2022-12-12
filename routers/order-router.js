ca03054c687132fc4ea3ccfefed2d5a9c089254d
abbas1902 <abbas1902@hotmail.ca>
2022-12-11 21:42:22 -0500

    billing info accessible in order page

--- routers/order-router.js
+++ routers/order-router.js
@@ -4,9 +4,37 @@ let router = express.Router();
 
 
 router
-    .get('/', getBooks)
+    .get('/', getUserInfo)
 
-function getBooks(req, res, next){
+
+function getUserInfo(req, res, next){
+	const { Pool } = require('pg')
+	const pool = new Pool({
+		host: 'localhost',
+		port: 5432,
+		user: 'postgres',
+		database: 'mainDB',
+		password: 'admin',
+	})
+	const query = {
+		text:'SELECT address,cardNumber,name FROM billingInfo WHERE username = $1;',
+		values: [req.session.username],
+		rowMode: 'array',
+	  }
+	pool.connect((err, client, done) => {
+		if (err) throw err
+		client.query(query, (err, result) => {
+		  if (err) {
+			console.log(err.stack)
+		  } else {
+			getBooks(res,req,result)
+		  }
+		})
+		client.release();
+	  })
+}
+
+	function getBooks(req, res, prevResult){
     const { Pool } = require('pg')
 	const pool = new Pool({
 		host: 'localhost',
@@ -25,23 +53,33 @@ function getBooks(req, res, next){
 		  if (err) {
 			console.log(err.stack)
 		  } else {
-			createList(res,result,req)
+			createList(res,req,prevResult,result)
 		  }
 		})
 		client.release();
 	  })
 }
-function createList(res,results,req){
+function createList(req,res,prevResult,results){
 	ISBNs=[]
 	Titles=[]
 	Prices=[]
 	Inventories=[]
+	Addresses=[]
+	CardNums=[]
+	Names=[]
 	for (let i = 0; i<results.rowCount;i++){
 		ISBNs.push(results.rows[i][0])
 		Titles.push(results.rows[i][1])
 		Prices.push(results.rows[i][2])
 		Inventories.push(results.rows[i][3])
 	}
+
+	for (let i = 0; i<prevResult.rowCount;i++){
+		CardNums.push(prevResult.rows[i][0])
+		Names.push(prevResult.rows[i][1])
+		Addresses.push(prevResult.rows[i][2])
+	}
+
 	let data = pug.renderFile("views/pages/books.pug", {
 		loggedin: req.session.loggedin, 
 		username: req.session.username,
@@ -49,7 +87,10 @@ function createList(res,results,req){
 		ISBNs: ISBNs,
 		Titles: Titles,
 		Prices:Prices,
-		Inventories:Inventories
+		Inventories:Inventories,
+		Addresses:Addresses,
+		CardNums:CardNums,
+		Names:Names
 	});
 	res.setHeader('Content-Type', 'text/html');
 	res.status(200).send(data);
